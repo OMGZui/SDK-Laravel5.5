f9bec5486845a50b0b68d37006bbbc1e8f1ec72f
omgzui <862275679@qq.com>
2017-12-06 21:04:04 +0800

    邮件集成

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\RegisterMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use OMGZui\OMGZui\Facades\OMGZui;
 
 class TestController extends Controller
@@ -11,5 +13,11 @@ class TestController extends Controller
     public function demo()
     {
         return view('test',['name' => OMGZui::getName(), 'title' => '小蠢货']);
+    }
+
+    public function send()
+    {
+        Mail::to('15679769443@163.com')->send(new RegisterMail());
+        return 'send ok';
     }
 }
--- routes/test.php
+++ routes/test.php
@@ -14,3 +14,4 @@ use Illuminate\Http\Request;
 */
 
 Route::get('/','TestController@demo');
+Route::get('/send','TestController@send');
